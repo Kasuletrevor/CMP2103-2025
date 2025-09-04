c07484711f265cc63a1f940c21e658a40e7b5cfd
Kasule John Trevor <82664582+Kasuletrevor@users.noreply.github.com>
2025-09-04 10:32:02 +0300

    Add eof_ex.cpp to demonstrate summation using user input until EOF

---
+++ functions/eof_ex.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int sum = 0;
+    int num;
+
+    // cin >> num;
+    do
+    {
+        cin >> num;
+        sum += num;
+
+    } while (!cin.eof());
+
+    cout << "Sum is " << sum << endl;
+
+    return 0;
+}
