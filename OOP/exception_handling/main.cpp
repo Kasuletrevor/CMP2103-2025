864e2dde0f9ed1a19ed1c221672dbf97d2ce2ff4
Kasule John Trevor <82664582+Kasuletrevor@users.noreply.github.com>
2025-10-30 11:05:22 +0300

    Add exception handling example for division with user input

---
+++ OOP/exception_handling/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cassert>
+
+using namespace std;
+
+int main()
+{
+
+    int dividend, divisor, quotient;
+
+    cout << "Enter dividend: ";
+    cin >> dividend;
+
+    cout << "Enter divisor: ";
+    cin >> divisor;
+
+    assert(divisor != 0);
+
+    quotient = dividend / divisor;
+
+    cout << "Quotient: " << quotient << endl;
+
+    return 0;
+}
